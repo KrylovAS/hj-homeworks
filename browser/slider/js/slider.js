0be7b223544560bbcb219010bc3e6f4b3752e10a
KrylovAS <s1nii@mail.ru>
2018-04-23 17:53:21 +0400

    реализован слайдер

---
+++ browser/slider/js/slider.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const img = document.getElementById('slider');
+const arrImage = ['i/airmax-jump.png', 'i/airmax-on-foot.png', 'i/airmax-playground.png', 'i/airmax-top-view.png', 'i/airmax.png' ]
+let step = 0;
+
+function pagingSlide() {
+    img.src = arrImage[step];
+    step += 1;
+    if(step > arrImage.length - 1){
+        step = 0;
+    }
+
+}
+
+setInterval(pagingSlide, 1000);
