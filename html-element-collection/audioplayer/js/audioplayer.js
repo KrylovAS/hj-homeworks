4d09afe62663a39d8bc84c570ddd51e3f162a1d6
KrylovAS <s1nii@mail.ru>
2018-04-26 20:19:53 +0400

    реализован аудиоплеер

---
+++ html-element-collection/audioplayer/js/audioplayer.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const player = document.getElementsByTagName('audio')[0];
+const playe = document.getElementsByTagName('audio');
+const titleMusic = document.getElementsByTagName('span')[0];
+const btnPlay = document.getElementsByClassName('playstate')[0];
+const btnStop = document.getElementsByClassName('stop')[0];
+const btnBack= document.getElementsByClassName('back')[0];
+const btnNext = document.getElementsByClassName('next')[0];
+const playstate = document.getElementsByClassName('playstate')[0];
+const arrMusic = ['mp3/LA Chill Tour.mp3', 'mp3/LA Fusion Jam.mp3', 'mp3/This is it band.mp3'];
+const arrTitle = ['LA Chill Tour', 'LA Fusion Jam', 'This is it band'];
+
+btnPlay.onclick = function() {
+  
+  if(player.paused) {
+    player.play();
+    playstate.classList.add('fa','fa-pause' );    
+  }else {
+    player.pause();
+    playstate.classList.remove('fa','fa-pause' );    
+  }
+}
+
+btnStop.onclick = function() {
+  player.pause();
+  player.currentTime = 0;  
+}
+let i = 0;
+btnNext.onclick = function() {
+  i++;
+
+  if(i > arrMusic.length - 1) {
+    i = 0;
+  }
+  
+  walkArray();   
+}
+
+btnBack.onclick = function() {
+  i--;
+
+  if(i < 0) {
+    i = arrMusic.length - 1;
+  }
+
+  walkArray()
+}
+
+function walkArray() {
+  player.src = arrMusic[i];
+  titleMusic.title = arrTitle[i]; 
+}
+
+
+
+
