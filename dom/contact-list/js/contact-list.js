b18240cdb4a5911f00bbb8013ea4fd80d090b2f1
KrylovAS <s1nii@mail.ru>
2018-05-12 17:41:09 +0400

    дал переменной более точное название, по хранящимся в ней данным

--- dom/contact-list/js/contact-list.js
+++ dom/contact-list/js/contact-list.js
@@ -2,15 +2,15 @@
 
 const userDate = JSON.parse(loadContacts());
 const userList = document.getElementsByClassName('contacts-list')[0];
-let userCard = '';
+let userCardsHTML = '';
 
 processingData();
 
-userList.innerHTML =  userCard;
+userList.innerHTML =  userCardsHTML;
 
 function processingData() {
   for (let persone of userDate) {
-    userCard += `<li data-email="${persone.email}" data-phone="${persone.phone}"><strong>${persone.name}</strong></li>`;
+    userCardsHTML += `<li data-email="${persone.email}" data-phone="${persone.phone}"><strong>${persone.name}</strong></li>`;
   }
 }
 
