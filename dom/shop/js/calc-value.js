6c20366f9bee2ba57f9fe5965ac7b741ee1c1c6a
KrylovAS <s1nii@mail.ru>
2018-05-12 17:59:25 +0400

    переделал(упростил) логику сложения общей суммы

--- dom/shop/js/calc-value.js
+++ dom/shop/js/calc-value.js
@@ -4,16 +4,16 @@ const addProduct = document.getElementsByClassName('add');
 const totalSum = document.getElementById('cart-total-price');
 const totalProduct = document.getElementById('cart-count');
 let product = 0;
-let priceProduct = [];
+let priceProduct = 0;
 
 for(let a of addProduct) {
   a.addEventListener('click', calcQuantity)
 }
 
 function calcQuantity() {  
-   priceProduct.push(+(this.dataset.price));    
-   totalSum.innerHTML = getPriceFormatted(priceProduct.reduce((a, b) => a + b ));
-   product++;
-   totalProduct.innerHTML = product;  
+  priceProduct += +this.dataset.price
+  totalSum.innerHTML = priceProduct;  
+  product++;
+  totalProduct.innerHTML = product;  
 }
 
