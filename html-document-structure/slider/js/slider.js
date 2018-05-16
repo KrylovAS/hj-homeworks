c37392ba91d9d1a828cc408e73ee79bb1fdfc895
KrylovAS <s1nii@mail.ru>
2018-05-16 18:54:41 +0400

    рефакторинг кода

--- html-document-structure/slider/js/slider.js
+++ html-document-structure/slider/js/slider.js
@@ -6,50 +6,57 @@ const firstBtn = document.querySelector('a[data-action="first"]');
 const lastBtn = document.querySelector('a[data-action="last"]');
 const slide = document.getElementsByClassName('slide');
 const slideList = document.querySelector('.slides')
-console.log(slideList)
+const currentSlide = document.querySelector('.slide-current');
+
 slide[0].classList.add('slide-current');
 prevBtn.classList.add('disabled');
 firstBtn.classList.add('disabled');
 
 for(let key of sliderNav){ 
- key.addEventListener('click', checkStatus)
+ key.addEventListener('click', checkAccessibility)
 }
 
-function checkStatus(){ 
-  if(!this.classList.contains('disabled') && this === nextBtn) {
-    chanchSlide('next')
-  }else if(!this.classList.contains('disabled') && this === prevBtn) {
-    chanchSlide('prev')
-  }else if(!this.classList.contains('disabled') && this === lastBtn){
-    for(let key of slide){
-      key.classList.remove('slide-current')
+function checkAccessibility(){ 
+  if(!this.classList.contains('disabled')){
+    switch(this){
+      case nextBtn:
+      chanchSlide(true);
+      break;
+      case prevBtn:
+      chanchSlide(false);
+      break;
+      case lastBtn:
+      deleteCurrientClass();
+      slideList.lastElementChild.classList.add('slide-current');
+      prevBtn.classList.remove('disabled');
+      firstBtn.classList.remove('disabled');
+      nextBtn.classList.add('disabled');
+      lastBtn.classList.add('disabled');
+      break;
+      case firstBtn:
+      deleteCurrientClass();
+      slideList.firstElementChild.classList.add('slide-current');    
+      nextBtn.classList.remove('disabled');
+      lastBtn.classList.remove('disabled');
+      prevBtn.classList.add('disabled');
+      firstBtn.classList.add('disabled');
+      break;
     }
-    slideList.lastElementChild.classList.add('slide-current')
-    prevBtn.classList.remove('disabled') || firstBtn.classList.remove('disabled')
-    nextBtn.classList.add('disabled') || lastBtn.classList.add('disabled')
-    console.log(slideList)
-  }else if(!this.classList.contains('disabled') && this === firstBtn){
-    for(let key of slide){
-      key.classList.remove('slide-current')
+  }
+  function deleteCurrientClass() {
+    for (let key of slide) {
+      key.classList.remove('slide-current');
     }
-    slideList.firstElementChild.classList.add('slide-current')
-    console.log(slideList.firstElementChild)
-    console.log(slideList)
-    //slideList.lastElementChild.classList.remove('slide-current')
-    nextBtn.classList.remove('disabled') || lastBtn.classList.remove('disabled')
-    prevBtn.classList.add('disabled') || firstBtn.classList.add('disabled')
-    
   }
 }
 function chanchSlide(click) {
     const currentSlide = document.querySelector('.slide-current');
-    const showSlide = (click === 'next')? currentSlide.nextElementSibling : currentSlide.previousElementSibling;
+    const showSlide = click? currentSlide.nextElementSibling : currentSlide.previousElementSibling;
     currentSlide.classList.remove('slide-current');
-    showSlide.classList.add('slide-current');    
-      
+    showSlide.classList.add('slide-current');       
     showSlide.nextElementSibling ? nextBtn.classList.remove('disabled') || lastBtn.classList.remove('disabled') : nextBtn.classList.add('disabled') || lastBtn.classList.add('disabled');
     showSlide.previousElementSibling ? prevBtn.classList.remove('disabled') || firstBtn.classList.remove('disabled') : prevBtn.classList.add('disabled') || firstBtn.classList.add('disabled');
-    console.log(slideList)
+    
 }
 
 
