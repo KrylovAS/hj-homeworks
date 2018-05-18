b901a0bb54544f15eb9eadd965b7cb4dff37cc5e
KrylovAS <s1nii@mail.ru>
2018-05-18 16:31:55 +0400

    реализовал работоспособность выпадающего меню

--- event-bubbling-capturing/menu/js/menu.js
+++ event-bubbling-capturing/menu/js/menu.js
@@ -1,6 +1,11 @@
 'use strict';
 
-function toggleMenu(event) {
+function toggleMenu(event) {  
+  const nodeDropdownMenu = Array
+                .from(document.querySelectorAll('a[data-toggle="dropdown"]'))
+                .find(el => el === event.target); 
+
+  if(event.target !== nodeDropdownMenu)  return;
   if (this.classList.contains('show')) {
     this.classList.remove('show');
     this.classList.add('hide');
@@ -11,14 +16,15 @@ function toggleMenu(event) {
 }
 
 function openLink(event) {
+  event.preventDefault();  
   console.log(this.textContent);
 }
 
-function init(node) {
+function init(node) {  
   node.addEventListener('click', toggleMenu);
 }
 
-function initLink(node) {
+function initLink(node) {  
   if (node.dataset.toggle) {
     return;
   }
