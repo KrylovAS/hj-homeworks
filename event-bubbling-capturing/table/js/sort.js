62d33cbf2c04e33c9df2cb05aa143bcd451e91ab
KrylovAS <s1nii@mail.ru>
2018-05-18 19:13:42 +0400

    реализовал таблицу с сортировкой

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function handleTableClick(event) {
-  
-}
+  let nodeTh = Array
+                    .from(document.getElementsByClassName('prop__name'))
+                    .find(el => el === event.target);  
+
+  if(event.target !== nodeTh ) return;
+
+  event.currentTarget.dataset.sortBy = event.target.dataset.propName;  
+  event.target.dataset.dir;   
+  (event.target.dataset.dir > 0)? event.target.dataset.dir = -1 : event.target.dataset.dir = 1;
+
+  sortTable(event.target.dataset.propName, event.target.dataset.dir );
+ }
+
+
