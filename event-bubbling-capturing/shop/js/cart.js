d9f713fcf39615492c5dce6db66acd390ea31f48
KrylovAS <s1nii@mail.ru>
2018-05-18 21:38:57 +0400

    реализовал добавление в корзину товаров

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,15 @@
 'use strict';
+const itemList = document.getElementsByClassName('items-list')[0];
+itemList.addEventListener('click', addBasket);
+
+function addBasket(event) {  
+  if(!event.target.classList.contains('add-to-cart')) return;
+  
+  let item = {};
+  item.title = event.target.dataset.title;
+  item.price = event.target.dataset.price;
+  console.log(event.target)
+  addToCart(item);
+  
+}
+
