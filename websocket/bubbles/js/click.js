f946001f454d0254e1503006f5e6958d75feeefa
KrylovAS <s1nii@mail.ru>
2018-05-25 20:36:34 +0400

    реализовал шарики

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const connect = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+document.addEventListener('click', e => {
+  e.preventDefault();
+  let a = {};
+  a.x = e.pageX;
+  a.y = e.pageY;
+  connect.send(JSON.stringify(a));  
+  showBubbles(connect)
+});
+
+
+
+
