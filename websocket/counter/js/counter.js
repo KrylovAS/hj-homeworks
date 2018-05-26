7298b19df49fa553bd0676a8c8c373504320257c
KrylovAS <s1nii@mail.ru>
2018-05-26 22:02:57 +0400

    реализовал счетчик соединений и ошибок

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,15 @@
 'use strict';
+const counter = document.getElementsByClassName('counter')[0];
+const errors = document.getElementsByClassName('errors')[0];
+const connect = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+connect.addEventListener('message', e => {  
+  let getData = JSON.parse(e.data);
+
+  counter.textContent = getData.connections ;
+  errors.textContent = getData.errors;  
+});
+
+window.addEventListener('beforeunload', () => {
+    connect.close(1000)
+});
