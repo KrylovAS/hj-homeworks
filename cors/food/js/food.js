66afd619c0e701404b149d98c5333308b032695a
KrylovAS <s1nii@mail.ru>
2018-05-31 01:10:09 +0400

    рефакторинг

--- cors/food/js/food.js
+++ cors/food/js/food.js
@@ -1,16 +1,19 @@
 'use strict';
 
-loadData('https://neto-api.herokuapp.com/food/42')
-.then(foodData)
-.then(loadData)
-.then(raitingData)
-.then(loadData)
-.then(userData)
+Promise.all([loadData('https://neto-api.herokuapp.com/food/42'),
+             loadData('https://neto-api.herokuapp.com/food/42/rating'),
+             loadData('https://neto-api.herokuapp.com/food/42/consumers')])
+.then(res => {
+  [foodData(res[0]),
+   raitingData(res[1]),
+   userData(res[2])]    
+})
+.catch(err => console.log(err));
 
 function loadData(url){
-  const callbackName = 'callback' + parseInt(Math.random() * ( 50- 1) + 1);
+  const callbackName = 'callback' + parseInt(Math.random() * (50- 1) + 1);
   return new Promise((done, fail) => {
-    window[callbackName] = done;
+    window[callbackName] = done;    
     const script = document.createElement('script');
     script.src = `${url}?jsonp=${callbackName}`;
     document.body.appendChild(script)
@@ -18,30 +21,55 @@ function loadData(url){
 }
 
 function foodData(data){
-document.querySelector('[data-pic]').style= `background-image: url(${data.pic})`;
+document.querySelector('[data-pic]').style = `background-image: url(${data.pic})`;
 document.querySelector('[data-title]').textContent = data.title;
 document.querySelector('[data-ingredients]').textContent = data.ingredients.join(',');
-
-
-return `https://neto-api.herokuapp.com/food/42/rating`;
 }
 
 function raitingData(data){
 document.querySelector('[data-rating]').textContent = data.rating.toFixed(2);
 document.querySelector('[data-votes]').textContent = `(${data.votes} оценок)`;
 document.querySelector('[data-star]').style = `width: ${data.rating.toFixed(2) * 10}%`;
-
-return `https://neto-api.herokuapp.com/food/42/consumers`
-
 }
 
 function userData(data){
-  document.querySelector('[data-consumers]').innerHTML = '';
-  data.consumers.forEach(el => {
-  document.querySelector('[data-consumers]').innerHTML += `<img src=${el.pic} title=${el.name}>`;
-  })
-  const span = document.createElement('span');
+  const span = document.createElement('span');    
+  data.consumers.forEach(user => {
+    document.querySelector('[data-consumers]').appendChild(createUser(userTemplate(user)));    
+  });
   span.textContent = `(+${data.total - data.consumers.length})`
   document.querySelector('[data-consumers]').appendChild(span);
+}
+
+function userTemplate(data) {
+  return { tag: 'img', attr: {src: data.pic, title: data.name }}
+}
+
+function createUser(user) {  
+  if ((user === undefined) || (user === null) || (user === false)) {
+      return document.createTextNode('');
+  }
+
+  if ((typeof user === 'string') || (typeof user === 'number') || (user === true)) {
+       return document.createTextNode(user);
+  }
+
+  if (Array.isArray(user)) {
+    return user.reduce((f, elem) => {
+      f.appendChild(createUser(elem));
+        return f;
+      }, document.createDocumentFragment());
+  }
+
+  const element = document.createElement(user.tag || 'div');
+   
+  if (user.attr) {
+    Object.keys(user.attr)
+          .forEach(key => element.setAttribute(key, user.attr[key]))
+  }
+
+  element.appendChild(createUser(user.content));
+
+  return element;
 }
 
