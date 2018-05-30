451a3960dbae0693e9c04699babd17252137d6ca
KrylovAS <s1nii@mail.ru>
2018-05-31 01:30:49 +0400

    рефакторинг

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,4 +1,9 @@
 'use strict';
+loadData('https://neto-api.herokuapp.com/profile/me')
+  .then(profileData)
+  .then(loadData)
+  .then(technologiesData)
+  .catch(err => console.log(err))
 
 function loadData(url){
   const callbackName = 'callback' + parseInt(Math.random() * (50 - 1) + 1);  
@@ -21,15 +26,39 @@ function profileData(data) {
 
 function technologiesData(data){  
   let  badgescard = document.getElementsByClassName('badgescard')[0];
-  document.getElementsByClassName('content')[0].style.display = 'initial';
-  badgescard.innerHTML= '';  
-  data.forEach(el => {
-    badgescard.innerHTML += `<span class="devicons devicons-${el}"></span>`  
-  });  
+  document.getElementsByClassName('content')[0].style = 'display: initial';    
+  data.forEach(user => {
+    badgescard.appendChild(createTech(createTamplate(user)));     
+  });
 }
 
-loadData('https://neto-api.herokuapp.com/profile/me')
-  .then(profileData)
-  .then(loadData)
-  .then(technologiesData)
-  .catch(err => console.log(err))
+function createTamplate(techData){
+  return { tag: 'span', cls: ['devicons', `devicons-${techData}`]}
+}
+
+function createTech(data) {  
+  if (!data) {
+      return document.createTextNode('');
+  }
+
+  if ((typeof data === 'string') || (typeof data === 'number') || (data === true)) {
+    return document.createTextNode(data);
+  }
+
+  if (Array.isArray(data)) {
+    return data.reduce((f, elem) => {
+      f.appendChild(createTech(elem));
+      return f;
+    }, document.createDocumentFragment());
+   }
+
+   const element = document.createElement(data.tag || 'div');
+   [].concat(data.cls || [])
+       .forEach(className => element.classList.add(className));   
+
+   element.appendChild(createTech(data.content));
+
+   return element;
+}
+
+
