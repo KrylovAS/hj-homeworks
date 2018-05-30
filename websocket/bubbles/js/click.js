3ece1af6558bbffd585440bf7b2e214596771514
KrylovAS <s1nii@mail.ru>
2018-05-30 15:25:48 +0400

    подправил логику

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -2,15 +2,13 @@
 
 const connect = new WebSocket('wss://neto-api.herokuapp.com/mouse');
 
-document.addEventListener('click', e => {
-  e.preventDefault();
+document.addEventListener('click', e => {  
   let a = {};
   a.x = e.pageX;
   a.y = e.pageY;
   connect.send(JSON.stringify(a));  
-  showBubbles(connect)
 });
 
-
+showBubbles(connect)
 
 
