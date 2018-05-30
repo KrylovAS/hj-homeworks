ebefc2f0d51203cf06282658d5d69cfdc38643aa
KrylovAS <s1nii@mail.ru>
2018-05-30 13:34:26 +0400

    изменил название переменной

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,20 +1,15 @@
  'use strict';
 function loadData(url) {
-  const functionName = randName();
+  const callbackName = 'callback' + parseInt(Math.random() * (50 - 1) + 1);
   return new Promise((done, fail) => {
   window[functionName] = done;
   const script = document.createElement('script');
-  script.src = `${url}?jsonp=${functionName}`;
+  script.src = `${url}?jsonp=${callbackName}`;
   document.body.appendChild(script);
   });
 }
 
-function randName() {
-  return 'callback' + parseInt(Math.random() * (50 - 1) + 1);  
-}
-
-function setData(data){
-  console.log(data)
+function setData(data){ 
   document.getElementsByClassName('bg')[0].setAttribute('src', data.wallpaper);
   document.querySelector('[data-username]').textContent = data.username;
   document.querySelector('[data-description]').textContent = data.description;
@@ -22,10 +17,8 @@ function setData(data){
   document.querySelector('[data-tweets]').textContent = data.tweets;
   document.querySelector('[data-followers]').textContent = data.followers;
   document.querySelector('[data-following]').textContent = data.following;
-
 }
-
-
 
  loadData('https://neto-api.herokuapp.com/twitter/jsonp')
   .then(setData)
+  .then(err => console.log(err))
