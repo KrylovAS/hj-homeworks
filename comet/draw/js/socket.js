4f5ad69970eae6feb6f751570ef25455f551776d
KrylovAS <s1nii@mail.ru>
2018-06-02 00:54:28 +0400

    реализовал подключение webSocket и передачи изображения

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,10 @@
 'use strict';
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+editor.addEventListener('update', e => {  
+  canvas.toBlob(img => ws.send(img));  
+});
+
+window.addEventListener('beforeunload' , () => {
+  ws.close();
+})
+
